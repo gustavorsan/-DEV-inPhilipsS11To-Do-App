a508eb2fbb32eaf67f1b8205328648faa5944fba
gustavorsan <gustavorsantos42@gmail.com>
2022-12-23 07:48:50 -0300

    Separação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { ToDoAppComponent } from './pages/to-do-app/to-do-app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InputComponent } from './pages/to-do-app/components/input/input.component';
+import { NavComponent } from './pages/to-do-app/components/nav/nav.component';
+import { TaskComponent } from './pages/to-do-app/components/task/task.component';
+import { ToDoListComponent } from './pages/to-do-app/components/to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoAppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InputComponent,
+    NavComponent,
+    TaskComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/to-do-app/components/task/task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tdapp-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+
+}
