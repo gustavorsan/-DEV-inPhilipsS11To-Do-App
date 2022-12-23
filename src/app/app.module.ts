12ab2a4556a6d64dcea0a124114b3cf875c875b1
gustavorsan <gustavorsantos42@gmail.com>
2022-12-23 07:23:06 -0300

    Estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoAppComponent } from './pages/to-do-app/to-do-app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoAppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
