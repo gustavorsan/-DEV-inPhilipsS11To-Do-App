44da66df843d1dea34dcd1111e7368660a3ea0aa
gustavorsan <gustavorsantos42@gmail.com>
2022-12-24 07:07:48 -0300

    Implementado toggle da tarefa

--- src/app/pages/to-do-app/components/task/task.component.ts
+++ src/app/pages/to-do-app/components/task/task.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class TaskComponent {
   @Input() name = '';
   @Input() completed = false;
+
+  toggleCompletion(){
+    this.completed = !this.completed;
+  }
 }
